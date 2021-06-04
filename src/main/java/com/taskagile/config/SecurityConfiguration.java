74404c41b2c23e98e99768d618cf8f2a78392f34
imdongjin <geniuslim27@gmail.com>
2021-06-04 11:23:41 +0900

    feat: PasswordEncryptorDelegator

--- src/main/java/com/taskagile/config/SecurityConfiguration.java
+++ src/main/java/com/taskagile/config/SecurityConfiguration.java
@@ -1,15 +1,23 @@
 package com.taskagile.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private static final String[] PUBLIC = new String[]{
       "/error", "/login", "/logout", "/register", "/api/registrations"
     };
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
+++ src/main/java/com/taskagile/domain/common/security/PasswordEncryptorDelegator.java
@@ -1,16 +1,18 @@
 package com.taskagile.domain.common.security;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PasswordEncryptorDelegator implements PasswordEncryptor {
-    // [TODO] 패스워드 암호화 구현하기 , spring security 패키지의 PasswordEncoder 적용
-    //    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    public PasswordEncryptorDelegator(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public String encrypt(String rawPassword) {
-        // return passwordEncoder.encode(rawPassword);
-//        return "test encrypt password";
-        throw new UnsupportedOperationException();
+         return passwordEncoder.encode(rawPassword);
     }
 }
